52800bc955c09a7ddf89afa28a65c99ac715477a
= <=>
2022-10-20 14:30:44 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+
+import App from './views/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App></App>
+    </React.StrictMode>
+  )
+  
