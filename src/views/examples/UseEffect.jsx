6bd1af428bcdc9999f8c2f31325f9410ce3df11a
= <=>
2022-10-20 15:02:00 -0300

    exemplo useEffect e useState

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-
+const calculateFatorial = (n) => {
+    if(n < 0) return -1;
+    else if(n === 0) return 1;
+    else return calculateFatorial(n - 1) * n;
+}
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1);
+    const [fatorial, setFatorial] = useState(1);
+    useEffect(()=>{
+        setFatorial(calculateFatorial(number));
+    },[number])
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <span>Fatorial: {fatorial}</span>
+            <input type="number" className="input" value={number} onChange={(e) => setNumber(e.target.value)} />
         </div>
     )
 }
--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+    const [counter, setCounter] = useState(0);
+    const [name, setName] = useState('');
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+
+            <SectionTitle title="Exercicio #01"></SectionTitle>
+            <div className="center">
+                <span className='text'>{counter}</span>
+                <div className="">
+                    <button className="btn" onClick={() => setCounter(counter-1)}>-1</button>    
+                    <button className="btn" onClick={() => setCounter(counter+1)}>+1</button>    
+                </div>
+            </div>
+            <SectionTitle title="Exercicio #02"></SectionTitle>
+            <input type="text" className='input' value={name} onChange={(e) => setName(e.target.value)}/>
+            <span className="">{name}</span>
         </div>
     )
 }
