8f921c664f335d0911741bace3a1abe63f2bf429
= <=>
2022-10-21 09:36:45 -0300

    useEffect e UseRef

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 const calculateFatorial = (n) => {
     if(n < 0) return -1;
     else if(n === 0) return 1;
     else return calculateFatorial(n - 1) * n;
 }
+const checkNumero = (n) => {
+    if(n % 2 === 0) return 'par';
+    return 'impar';
+}
 const UseEffect = (props) => {
     const [number, setNumber] = useState(1);
     const [fatorial, setFatorial] = useState(1);
+    const [numero, setNumero] = useState(1);
+    const [tipoNumero, setTipoNumero] = useState('par');
     useEffect(()=>{
         setFatorial(calculateFatorial(number));
     },[number])
+    
+    useEffect(()=>{
+        setTipoNumero(checkNumero(numero));
+    },[numero])
     return (
         <div className="UseEffect">
             <PageTitle
@@ -21,6 +32,10 @@ const UseEffect = (props) => {
             />
             <span>Fatorial: {fatorial}</span>
             <input type="number" className="input" value={number} onChange={(e) => setNumber(e.target.value)} />
+            <SectionTitle title="Exercicio 02"></SectionTitle>
+
+            <span>Tipo numero: {tipoNumero}</span>
+            <input type="number" className="input" value={numero} onChange={(e) => setNumero(e.target.value)} />
         </div>
     )
 }
--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-
+import SectionTitle from '../../components/layout/SectionTitle'
+const MergeString = (str1, str2) => {
+    return [...str1].map((e, i)=>{
+        return `${e}${str2[i] || ""}`;
+    }).join("");
+}
 const UseRef = (props) => {
+    const [value,setValue] = useState('');
+    const [value1,setValue1] = useState('');
+    const [value2,setValue2] = useState('');
+    const count = useRef(0);
+
+    const myInput1 = useRef(null);
+    const myInput2 = useRef(null);
+    useEffect(() => {
+        count.current = count.current + 1;    
+    },[value])
+    
+    useEffect(() => {
+        myInput2.current.focus();    
+    },[value1])
+    
+    useEffect(() => {
+        myInput1.current.focus();    
+    },[value2])
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="exercicio 1"></SectionTitle>
+            <span className='text'>Contador: {count.current}</span>
+            <span className='text'>Valor: {value}</span>
+            <input type="text" className="input" value={value} onChange={(e)=> setValue(e.target.value)}/>
+
+            <SectionTitle title="exercicio #02"></SectionTitle>
+            <span className='text'>{MergeString(value1, value2)}</span>
+            <input ref={myInput1} type="text" className="input" value={value1} onChange={(e)=> setValue1(e.target.value)}/>
+            <input ref={myInput2} type="text" className="input" value={value2} onChange={(e)=> setValue2(e.target.value)}/>
         </div>
     )
 }
